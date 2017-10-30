d6322eafdd3d0a468391d49330ae1b7b89a517b6
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2017-10-31 02:39:17 +0900

    Add my settings.

--- build.sh
+++ build.sh
@@ -16,14 +16,16 @@ rm -f config.status
 # extracflags="-O3 -march=native -Wall -D_REENTRANT -funroll-loops -fvariable-expansion-in-unroller -fmerge-all-constants -fbranch-target-load-optimize2 -fsched2-use-superblocks -falign-loops=16 -falign-functions=16 -falign-jumps=16 -falign-labels=16"
 
 # Debian 7.7 / Ubuntu 14.04 (gcc 4.7+)
-extracflags="$extracflags -Ofast -flto -fuse-linker-plugin -ftree-loop-if-convert-stores"
+extracflags="$extracflags -Ofast -ftree-loop-if-convert-stores -ftree-vectorize"
 
 if [ ! "0" = `cat /proc/cpuinfo | grep -c avx` ]; then
     # march native doesn't always works, ex. some Pentium Gxxx (no avx)
     extracflags="$extracflags -march=native"
 fi
 
-./configure --with-crypto --with-curl CFLAGS="-O2 $extracflags -DUSE_ASM -pg"
+extracflags="$extracflags -g -DSPH_KECCAK_UNROLL=1 -DSPH_CUBEHASH_UNROLL=2 -DSPH_CUBEHASH_NOCOPY=1"
+
+./configure --with-crypto --with-curl CFLAGS="-O2 $extracflags -DUSE_ASM"
 
 make -j 4
 
